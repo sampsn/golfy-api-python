09ed7897e7f16bd268e5ca7fe60b6248cbf71348
sampsn <g@samp.sn>
2024-04-04 10:37:16 -0600

    Add main endpoints

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
+from uuid import UUID
+
 from fastapi import FastAPI
 
+from models.course import Course
+from models.player import Player
+
 app = FastAPI()
+
+
+players: dict[UUID, Player] = {}
+courses: dict[UUID, Course] = {}
+
+@app.get("/players")
+# @app.post("/players")
+# @app.put("/players/{player_id}")
+# @app.delete("/players/{player_id}")
+
+@app.get("/courses")
+# @app.post("/courses")
+# @app.put("/courses/{courses_id}")
+# @app.delete("/courses/{courses_id}")
--- models/player.py
+++ models/player.py
@@ -1,1 +1,8 @@
+from uuid import UUID
+
 from pydantic import BaseModel
+
+
+class Player(BaseModel):
+    id: UUID
+    name: str
